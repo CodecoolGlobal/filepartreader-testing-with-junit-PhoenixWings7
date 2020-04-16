e76fc0924e9484d04482ec5afbe46f97d8348898
Klaudia Adamus <adamus.klaudia.pasp@gmail.com>
2020-04-16 22:57:44 +0200

    created basic project layout and UML diagram

---
+++ src/main/java/FilePartReader.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class FilePartReader {
+    public void setup(String flePath, Integer fromLine, Integer toLine) throws IllegalArgumentException {
+    }
+
+    public String read() throws IOException {
+        return null;
+    }
+
+    public String readLines() {
+        return null;
+    }
+}
---
+++ src/main/java/FileWordAnalyzer.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class FileWordAnalyzer {
+    public FileWordAnalyzer(FilePartReader filePartReader) {
+    }
+
+    public List getWordsOrderedAlphabetically() {
+        return null;
+    }
+
+    public List getWordsContainingSubstring(String subString) {
+        return null;
+    }
+
+    public List getStringsWhichPalindromes() {
+        return null;
+    }
+}
